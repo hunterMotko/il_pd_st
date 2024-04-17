365da30fac5c2f9a1890da9bcc6e58164ed837b9
hunterMotko <huntermotko@gmail.com>
2024-04-17 16:30:33 -0400

    remove intial query asnyc

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,25 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 20,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: process.env.CERT
+export const pool = (() => {
+    return new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+})();
+
+export const client = async (text: string, params: Params[] = []) => {
+  try {
+    const c = await pool.connect()
+    const res = await c.query(text, params)
+    c.release()
+    return res
+  } catch (error) {
+    console.error(error)
   }
-});
+}
 
 type Params = string | number | boolean | Date | null | undefined;
 
-export const query = async (text: string, params: Params[] = []) => await pool.query(text, params);
+export const query = (text: string, params: Params[] = []) => pool.query(text, params);
