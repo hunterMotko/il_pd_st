15206afb01c9c586bdad6109fe704e8920fa9b5c
hunterMotko <huntermotko@gmail.com>
2025-01-19 10:59:47 -0500

    now

--- src/app/counties/page.tsx
+++ src/app/counties/page.tsx
@@ -1,8 +1,20 @@
 import CountyMap from "@/components/counties/CountyMap";
 import { feature } from "topojson-client";
 import topo from '../../../public/counties-topo.json';
+import { client } from "@/lib/db";
+
+async function getNow() {
+  try {
+    const results = await client('SELECT NOW()')
+    const rows = results?.rows
+    console.log(rows)
+  } catch (error) {
+    console.log('query ERR: ', error)
+  }
+}
 
 export default async function CountiesPage() {
+  getNow()
   const json = JSON.parse(JSON.stringify(topo))
   const topoJson = feature(json, json.objects.counties);
 
