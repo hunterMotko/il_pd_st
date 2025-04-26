b48354599593ff3c16c0f653ce8cf329bb6c30b5
hunterMotko <huntermotko@gmail.com>
2025-04-26 15:19:31 -0400

    fix: home style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import CardGroup from "@/components/home/CardGroup";
 
 export default function Home() {
   return (
-    <div className="h-lvh bg-gradient-to-b from-purple-900 from-30% via-white to-purple-900 sm:h-fit py-5">
-      <div className="h-1/3 text-center my-10 py-5">
-        <h2 className="text-4xl text-white font-extrabold mx-auto md:text-5xl">
+    <div className="h-screen bg-gradient-to-b from-purple-900 from-30% via-white to-purple-900 py-5">
+      <div className="text-center my-10 py-5">
+        <h2 className="text-5xl text-white font-extrabold mx-auto">
           Illinois Public Defender Statistics
         </h2>
       </div>
